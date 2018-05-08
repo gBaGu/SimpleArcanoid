fa385931a237bb1d11b0996ff55e24faec1d5463
Vadim <vadim.pysarenko97@gmail.com>
2018-05-08 12:31:34 +0300

    added check for overlapping (ball center is inside of brick/paddle)

--- SFMLGame/Ball.cpp
+++ SFMLGame/Ball.cpp
@@ -11,7 +11,8 @@ const float Ball::DEFAULT_RADIUS = 10.0f;
 const float Ball::DEFAULT_VELOCITY = 8.0f;
 
 Ball::Ball(float x, float y)
-	: CircleShape(DEFAULT_RADIUS), velocity(-DEFAULT_VELOCITY, -DEFAULT_VELOCITY)
+	: CircleShape(DEFAULT_RADIUS),
+	velocity(-DEFAULT_VELOCITY, -DEFAULT_VELOCITY)
 {
 	setPosition(x, y);
 	setFillColor(sf::Color::Red);
@@ -23,28 +24,43 @@ void Ball::update()
 	move(velocity);
 
 	if (getMinX() < 0)
+	{
 		velocity.x = DEFAULT_VELOCITY;
+	}
 	else if (getMaxX() > WINDOW_WIDTH)
+	{
 		velocity.x = -DEFAULT_VELOCITY;
+	}
 
 	if (getMinY() < 0)
+	{
 		velocity.y = DEFAULT_VELOCITY;
+	}
 	else if (getMaxY() > WINDOW_HEIGHT)
+	{
 		velocity.y = -DEFAULT_VELOCITY;
+	}
 }
 
 bool Ball::checkCollision(const sf::RectangleShape& p)
 {
 	if (!isIntersecting(p))
+	{
 		return false;
+	}
 
+	//TODO: recalculate velocity vector
 	auto ballVelocity = getVelocity();
 	ballVelocity.y = -ballVelocity.y;
 
 	if (getPosition().x < p.getPosition().x)
+	{
 		ballVelocity.x = -Ball::DEFAULT_VELOCITY;
+	}
 	else if (getPosition().x > p.getPosition().x)
+	{
 		ballVelocity.x = Ball::DEFAULT_VELOCITY;
+	}
 
 	setVelocity(ballVelocity);
 	return true;
@@ -52,6 +68,13 @@ bool Ball::checkCollision(const sf::RectangleShape& p)
 
 bool Ball::isIntersecting(const sf::RectangleShape& p) const
 {
+	auto center = getPosition();
+	auto radius = getRadius();
+	if (isInsideByCrossingNumber(center, p))
+	{
+		return true;
+	}
+
 	std::vector<sf::Vector2f> paddlePoints;
 	for (int i = 0; i < p.getPointCount(); i++)
 	{
@@ -60,17 +83,17 @@ bool Ball::isIntersecting(const sf::RectangleShape& p) const
 		paddlePoints.push_back(transform.transformPoint(point));
 	}
 	if (paddlePoints.empty())
+	{
 		return false;
+	}
 
 	std::vector<Segment<float>> paddleSides;
-	for (auto it = paddlePoints.begin(); it != paddlePoints.end() - 1 && it != paddlePoints.end(); it++)
+	for (auto it = paddlePoints.begin(); it != std::prev(paddlePoints.end()); it++)
 	{
 		paddleSides.emplace_back(*it, *(it + 1));
 	}
 	paddleSides.emplace_back(paddlePoints.back(), paddlePoints.front());
 
-	auto center = getPosition();
-	auto radius = getRadius();
 	return std::any_of(paddleSides.begin(), paddleSides.end(),
 		[center, radius](Segment<float> s) { return distance(center, s) < radius; });
 }
--- SFMLGame/MyMath.h
+++ SFMLGame/MyMath.h
@@ -12,6 +12,25 @@ struct Segment
 };
 
 template <typename T>
+struct Line
+{
+	Line(sf::Vector2<T> p1, sf::Vector2<T> p2) : A(p1), B(p2) {}
+
+	sf::Vector2<T> A;
+	sf::Vector2<T> B;
+};
+
+template <typename T>
+struct Ray
+{
+	Ray(sf::Vector2<T> p1, sf::Vector2<T> p2) : A(p1), B(p2) {}
+
+	sf::Vector2<T> A;
+	sf::Vector2<T> B;
+};
+
+
+template <typename T>
 T length(sf::Vector2<T> s)
 {
 	return std::pow(s.x * s.x + s.y * s.y, 0.5);
@@ -38,18 +57,207 @@ T distance(sf::Vector2<T> p, Segment<T> s)
 	T scalarA = scalarMultiplication(segmentVector, vectorAtoPoint);
 	T scalarB = scalarMultiplication(segmentVector, vectorBtoPoint);
 
-	//если проекция на прямую, на которой лежит отрезок, находится вне отрезка (скалярная магия),
+	//если проекция на прямую, на которой лежит отрезок,
+	//находится вне отрезка (скалярная магия),
 	//возвращаем расстояние до ближайшей точки отрезка
 	if (scalarA < 0 && scalarB < 0 ||
 		scalarA > 0 && scalarB > 0)
 	{
-		sf::Vector2<T>& vectorToPoint = (std::abs(scalarA) < std::abs(scalarB) ? vectorAtoPoint : vectorBtoPoint);
-		return length(vectorToPoint);
+		if (std::abs(scalarA) < std::abs(scalarB))
+		{
+			return length(vectorAtoPoint);
+		}
+		else
+		{
+			return length(vectorBtoPoint);
+		}
 	}
 	else
 	{
 		auto unitVector = segmentVector / length(segmentVector);
-		sf::Vector3<T> geometricMul(0, 0, unitVector.x * vectorAtoPoint.y - unitVector.y * vectorAtoPoint.x);
+		sf::Vector3<T> geometricMul(0, 0, unitVector.x * vectorAtoPoint.y -
+										  unitVector.y * vectorAtoPoint.x);
 		return length(geometricMul);
 	}
+}
+
+//checks if point lies on a line
+template <typename T>
+bool isContaining(Line<T> l, sf::Vector2<T> p)
+{
+	return (p.x - l.A.x) * (l.B.y - l.A.y) == (l.B.x - l.A.x) * (p.y - l.A.y);
+}
+
+//checks if point lies on a ray
+template <typename T>
+bool isContaining(Ray<T> r, sf::Vector2<T> p)
+{
+	return isContaining(Line<T>(r.A, r.B), p) &&
+			((p.x - r.A.x) * (r.B.x - r.A.x) >= 0 &&
+				(p.y - r.A.y) * (r.B.y - r.A.y) >= 0);
+}
+
+//checks if point lies on a segment
+template <typename T>
+bool isContaining(Segment<T> s, sf::Vector2<T> p)
+{
+	return isContaining(Line(s.A, s.B), p) && isInside(p, s.A, s.B);
+}
+
+//checks if point is inside of rectangle created by two points
+template <typename T>
+bool isInside(sf::Vector2<T> p, sf::Vector2<T> boundP1, sf::Vector2<T> boundP2)
+{
+	return (p.x >= std::min(boundP1.x, boundP2.x) &&
+			p.x <= std::max(boundP1.x, boundP2.x)) &&
+		(p.y >= std::min(boundP1.y, boundP2.y) &&
+			p.y <= std::max(boundP1.y, boundP2.y));
+}
+
+//checks if line divides two points
+template <typename T>
+bool isDividing(Segment<T> s, sf::Vector2<T> p1, sf::Vector2<T> p2)
+{
+	//this is z coordinate of a result of vector multiplication
+	//of vector {s.A, s.B} and {s.B, p1}
+	T z1 = (s.B.x - s.A.x) * (p1.y - s.B.y) - (s.B.y - s.A.y) * (p1.x - s.B.x);
+	//this is z coordinate of a result of vector multiplication
+	//of vector {s.A, s.B} and {s.B, p2}
+	T z2 = (s.B.x - s.A.x) * (p2.y - s.B.y) - (s.B.y - s.A.y) * (p2.x - s.B.x);
+
+	return std::min(z1, z2) <= 0 && std::max(z1, z2) >= 0;
+}
+
+//check if one line is parallel to another
+template <typename T>
+bool isParallel(Line<T> l1, Line<T> l2)
+{
+	T A1, B1, A2, B2;
+	A1 = l1.B.y - l1.A.y;
+	B1 = l1.A.x - l1.B.x;
+	A2 = l2.B.y - l2.A.y;
+	B2 = l2.A.x - l2.B.x;
+
+	return A1 * B2 == B1 * A2;
+}
+
+//check if two points of first segment are on different sides
+//of second segment and vice versa
+template <typename T>
+bool isIntersected(Segment<T> s1, Segment<T> s2)
+{
+	return isDividing(s1, s2.A, s2.B) && isDividing(s2, s1.A, s1.B);
+}
+
+//returns an intersection point of two lines if there is one
+//bool indicates if an intersection was found
+//sf::Vector2<T> contains intersection point if bool is true
+template <typename T>
+std::pair<bool, sf::Vector2<T>> getIntersectionPoint(Line<T> l1, Line<T> l2)
+{
+	if (isParallel(l1, l2))
+	{
+		return std::pair<bool, sf::Vector2<T>>(false, sf::Vector2<T>{ 0, 0 });
+	}
+
+	T A1, B1, C1, A2, B2, C2;
+	A1 = l1.B.y - l1.A.y;
+	B1 = l1.A.x - l1.B.x;
+	C1 = l1.A.y * (l1.B.x - l1.A.x) - l1.A.x * (l1.B.y - l1.A.y);
+	A2 = l2.B.y - l2.A.y;
+	B2 = l2.A.x - l2.B.x;
+	C2 = l2.A.y * (l2.B.x - l2.A.x) - l2.A.x * (l2.B.y - l2.A.y);
+
+	T x = (C1 * B2 - B1 * C2) / (B1 * A2 - A1 * B2);
+	T y = -(A1 * x + C1) / B1;
+	return std::pair<bool, sf::Vector2<T>>(true, sf::Vector2<T>{ x, y });
+}
+
+//returns an intersection point of ray and segment if there is one
+//bool indicates if an intersection was found
+//sf::Vector2<T> contains intersection point if bool is true
+template <typename T>
+std::pair<bool, sf::Vector2<T>> getIntersectionPoint(Ray<T> r, Segment<T> s)
+{
+	auto p = getIntersectionPoint(Line<T>(r.A, r.B), Line<T>(s.A, s.B));
+	if (!p.first)
+	{
+		return p;
+	}
+
+	//if not on ray on not on segment there is no intersection
+	if (!((p.second.x - r.A.x) * (r.B.x - r.A.x) >= 0 &&
+			(p.second.y - r.A.y) * (r.B.y - r.A.y) >= 0) ||
+		!isInside(p.second, s.A, s.B))
+	{
+		p.first = false;
+	}
+	return p;
+}
+
+template <typename T>
+bool isInsideByCrossingNumber(sf::Vector2<T> p, const sf::RectangleShape& rect)
+{
+	sf::Vector2<T> rayVector(1, 0);
+
+	std::vector<sf::Vector2<T>> points;
+	for (int i = 0; i < rect.getPointCount(); i++)
+	{
+		auto point = rect.getPoint(i);
+		auto transform = rect.getTransform();
+		point = transform.transformPoint(point);
+		//if point lies on one of the vertices
+		//there is no need to count crossings
+		if (point == p)
+		{
+			return true;
+		}
+		points.push_back(point);
+	}
+
+	const auto firstPointNotOnRayIt = std::find_if_not(points.begin(), points.end(),
+		[=](sf::Vector2<T> vertex) { return isContaining(Ray<T>(p, p + rayVector), vertex); });
+	//this means that ALL of the vertices lie on the ray
+	//considering point doesn`t lie on any of the vertices. See the cycle above
+	if (firstPointNotOnRayIt == points.end())
+	{
+		return false;
+	}
+
+	auto pointIt = firstPointNotOnRayIt;
+	size_t crossings = 0;
+	do
+	{
+		auto nextPointIt = std::find_if_not(pointIt + 1, points.end(),
+			[=](sf::Vector2<T> vertex) { return isContaining(Ray<T>(p, p + rayVector), vertex); });
+		if (nextPointIt == points.end())
+		{
+			nextPointIt = firstPointNotOnRayIt;
+		}
+
+		sf::Vector2<T> p1 = *pointIt;
+		sf::Vector2<T> p2 = *nextPointIt;
+		//if ray does not intersecting any vertices between p1 and p2
+		//check for intersection between ray and segment (p1, p2)
+		if (nextPointIt == pointIt + 1 ||
+			(pointIt == std::prev(points.end()) && nextPointIt == points.begin()))
+		{
+			if (getIntersectionPoint(Ray<T>(p, p + rayVector), Segment<T>(p1, p2)).first)
+			{
+				crossings += 1;
+			}
+		}
+		//we know that ray intersects one or more vertice between p1 and p2
+		//so we must just check if p1 and p2 are on different sides of ray
+		else
+		{
+			if (isDividing(Segment<T>(p, p + rayVector), p1, p2))
+			{
+				crossings += 1;
+			}
+		}
+		pointIt = nextPointIt;
+	} while (pointIt != firstPointNotOnRayIt);
+
+	return crossings % 2;
 }
